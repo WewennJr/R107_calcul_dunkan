4dafcf89d498532ded943bf466c2050dfd795fc4
Haruto <dunkantoutain27@gmail.com>
2025-11-19 09:16:10 +0100

    version 1

---
+++ calcul_prenom.py
@@ -0,0 +1,28 @@
+def coef():
+    r101 = float(input("Quelles notes avez vous obtenu pour la ressources r101 dans votre UE1"))
+    r102 = float(input("Quelles notes avez vous obtenu pour la ressources r102 dans votre UE1"))
+    r103 = float(input("Quelles notes avez vous obtenu pour la ressources r103 dans votre UE1"))
+    r104 = float(input("Quelles notes avez vous obtenu pour la ressources r104 dans votre UE1"))
+    r105 = float(input("Quelles notes avez vous obtenu pour la ressources r105 dans votre UE1"))
+    r106 = float(input("Quelles notes avez vous obtenu pour la ressources r106 dans votre UE1"))
+    r107 = float(input("Quelles notes avez vous obtenu pour la ressources r107 dans votre UE1"))
+    r108 = float(input("Quelles notes avez vous obtenu pour la ressources r108 dans votre UE1"))
+    r109 = float(input("Quelles notes avez vous obtenu pour la ressources r109 dans votre UE1"))
+    r110 = float(input("Quelles notes avez vous obtenu pour la ressources r110 dans votre UE1"))
+    r111 = float(input("Quelles notes avez vous obtenu pour la ressources r111 dans votre UE1"))
+    r112 = float(input("Quelles notes avez vous obtenu pour la ressources r112 dans votre UE1"))
+    r113 = float(input("Quelles notes avez vous obtenu pour la ressources r113 dans votre UE1"))
+    r114 = float(input("Quelles notes avez vous obtenu pour la ressources r114 dans votre UE1"))
+    r115 = float(input("Quelles notes avez vous obtenu pour la ressources r115 dans votre UE1"))
+    SAE11 = float(input("Quelles notes avez vous obtenu pour la ressources SAE11 dans votre UE1"))
+    SAE12 = float(input("Quelles notes avez vous obtenu pour la ressources SAE12 dans votre UE1"))
+    SAE13 = float(input("Quelles notes avez vous obtenu pour la ressources SAE13 dans votre UE1"))
+    SAE14 = float(input("Quelles notes avez vous obtenu pour la ressources SAE14 dans votre UE1"))
+    SAE15 = float(input("Quelles notes avez vous obtenu pour la ressources SAE15 dans votre UE1"))
+    SAE16 = float(input("Quelles notes avez vous obtenu pour la ressources SAE16 dans votre UE1"))
+    UE1 = ((r101*10) + (r102*10) + (r103*7) + (r104*7) + (r106*5) + (r108*6) + (r110*5) + (r111*4) + (r112*2) + (r113*5) + (r114*5) + (SAE11*20) + (SAE12*20) + (SAE16*7))//113
+    print(UE1)
+
+coef()
+    
+    
